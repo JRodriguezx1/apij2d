153c6456b54d616e7bf496d6a09911359eb98ca2
Julian Rodriguez <julianlink123@hotmail.com>
2025-04-10 17:58:15 -0500

    organizando formulario de configuracion company

--- views/admin/configuracion/compañia.php
+++ views/admin/configuracion/compañia.php
@@ -2,10 +2,10 @@
     <h4 class="text-slate-500 text-2xl mb-4"><i class="fa-solid fa-building-circle-check mr-4"></i>Crear Compañia</h4>
     
 
-    <form class="max-w-md mx-auto">
+    <form class="max-w-xl mx-auto">
     <div class="mb-5">
-        <label for="type_document_identification_id" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">Tipo Documento</label>
-        <select id="type_document_identification_id" name="type_document_identification_id" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        <label for="type_document_identification_id" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Tipo Documento</label>
+        <select id="type_document_identification_id" name="type_document_identification_id" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
             <option value="1">Registro civil</option>
             <option value="2">Tarjeta de identidad</option>
             <option value="3">Cedula de ciudadania</option>
@@ -19,16 +19,78 @@
         </select>
     </div>
     <div class="mb-5">
-        <label for="identification_number" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">Numero de Documento</label>
-        <input type="number" id="identification_number" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
+        <label for="identification_number" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Numero de Documento</label>
+        <input type="number" id="identification_number" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Sin digito verificador" required />
     </div>
     <div class="mb-5">
-        <label for="password" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">ID Software</label>
-        <input type="number" id="password" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+        <label for="idsoftware" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">ID Software</label>
+        <input type="number" id="idsoftware" name="idsoftware" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
     </div>
     <div class="mb-5">
-        <label for="repeat-password" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">Pin Software</label>
-        <input type="password" id="repeat-password" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+        <label for="pinsoftware" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Pin Software</label>
+        <input type="number" id="pinsoftware" name="pinsoftware" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+    </div>
+    <div class="mb-5">
+        <label for="nombrerazonsocial" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Nombre/Razon Social</label>
+        <input type="text" id="nombrerazonsocial" name="nombrerazonsocial" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+    </div>
+    <div class="mb-5">
+        <label for="tipoorganizacion" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Tipo Organizacion</label>
+        <select id="tipoorganizacion" name="tipoorganizacion" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="1">Persona Juridica</option>
+            <option value="2">Persona Natural</option>
+        </select>
+    </div>
+    <div class="mb-5">
+        <label for="Obligaciones" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Obligaciones</label>
+        <select id="Obligaciones" name="Obligaciones" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="1">Gran contribuyente</option>
+            <option value="2">Autorretenedor</option>
+            <option value="3">Agente de retencion en el impuesto sobre las ventas</option>
+            <option value="4">Regimen simple de tributacion - Simple</option>
+            <option value="4">No Responsable</option>
+        </select>
+    </div>
+    <div class="mb-5">
+        <label for="impuesto" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Impuesto</label>
+        <select id="impuesto" name="impuesto" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="1">Ninguno</option>
+            <option value="2">IVA</option>
+            <option value="3">Impuesto Nacional al consumo</option>
+        </select>
+    </div>
+    <div class="mb-5">
+        <label for="regimen" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Regimen</label>
+        <select id="regimen" name="regimen" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="1">Responsable de IVA</option>
+            <option value="2">No Responsable de IVA</option>
+        </select>
+    </div>
+    <div class="mb-5">
+        <label for="email" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Correo electronico</label>
+        <input type="email" id="email" name="email" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+    </div>
+    <div class="mb-5">
+        <label for="telefono" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Telefono</label>
+        <input type="number" id="telefono" name="telefono" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+    </div>
+    <div class="mb-5">
+        <label for="departamento" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Departamento</label>
+        <select id="departamento" name="departamento" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="1">Responsable de IVA</option>
+            <option value="2">No Responsable de IVA</option>
+        </select>
+    </div>
+    <div class="mb-5">
+        <label for="Ciudad" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Ciudad</label>
+        <select id="Ciudad" name="Ciudad" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="1">Responsable de IVA</option>
+            <option value="2">No Responsable de IVA</option>
+        </select>
+    </div>
+    <div class="mb-5">
+        <label for="direccion" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Direccion</label>
+        <input type="text" id="direccion" name="direccion" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
     </div>
     
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 text-center ">Crear Compañia</button>
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -71,23 +71,23 @@
 </aside>-->
 
 <aside
-    class="fixed top-0 left-0 z-40 w-2xs h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    class="fixed top-0 left-0 z-40 w-2xs h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0  "
     aria-label="Sidenav"
     id="drawer-navigation"
     >
-      <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800">
+      <div class="overflow-y-auto py-5 px-3 h-full bg-white ">
         
-        <ul class="space-y-2">
+        <ul class="space-y-2 pt-4">
           <li>
             <button
               type="button"
-              class="flex items-center p-2 w-full text-xl font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              class="flex items-center p-2 w-full text-xl font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100  "
               aria-controls="dropdown-pages"
               data-collapse-toggle="dropdown-pages"
             >
               <svg
                 aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900  "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -107,26 +107,26 @@
             </button>
             <ul id="dropdown-pages" class="hidden py-2 space-y-2">
               <li>
-                <a href="/admin/configuracion/company" class="flex items-center p-2 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Compañia</a>
+                <a href="/admin/configuracion/company" class="flex items-center p-2 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100  ">Compañia</a>
               </li>
               <li>
-                <a href="#" class="flex items-center p-2 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Set de pruebas</a>
+                <a href="#" class="flex items-center p-2 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100  ">Set de pruebas</a>
               </li>
               <li>
-                <a href="#" class="flex items-center p-2 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Software</a>
+                <a href="#" class="flex items-center p-2 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100  ">Software</a>
               </li>
             </ul>
           </li>
           <li>
             <button
               type="button"
-              class="flex items-center p-2 w-full text-xl font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              class="flex items-center p-2 w-full text-xl font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100  "
               aria-controls="dropdown-sales"
               data-collapse-toggle="dropdown-sales"
             >
               <svg
                 aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900  "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -146,15 +146,15 @@
             </button>
             <ul id="dropdown-sales" class="hidden py-2 space-y-2">
               <li>
-                <a href="#" class="flex items-center p-2 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
+                <a href="#" class="flex items-center p-2 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100  ">Invoice</a>
               </li>
             </ul>
           </li>
           <li>
-            <a href="#" class="flex items-center p-2 text-xl font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <a href="#" class="flex items-center p-2 text-xl font-medium text-gray-900 rounded-lg  hover:bg-gray-100  group">
               <svg
                 aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -168,12 +168,12 @@
           </li>
           
         </ul>
-        <ul class="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
+        <ul class="pt-5 mt-5 space-y-2 border-t border-gray-200 ">
           <li>
-            <a href="#" class="flex items-center p-2 text-lg font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+            <a href="#" class="flex items-center p-2 text-lg font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100   group">
               <svg
                 aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -185,10 +185,10 @@
             </a>
           </li>
           <li>
-            <a href="#" class="flex items-center p-2 text-lg font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+            <a href="#" class="flex items-center p-2 text-lg font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100   group">
               <svg
                 aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -199,10 +199,10 @@
             </a>
           </li>
           <li>
-            <a href="#" class="flex items-center p-2 text-lg font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+            <a href="#" class="flex items-center p-2 text-lg font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100   group">
               <svg
                 aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -215,8 +215,8 @@
         </ul>
       </div>
 
-      <div class="hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white dark:bg-gray-800 z-20">
-        <a href="#" class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600">
+      <div class="hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white  z-20">
+        <a href="#" class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer  hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600">
           <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
           </svg>
@@ -224,7 +224,7 @@
         <a
           href="#"
           data-tooltip-target="tooltip-settings"
-          class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
+          class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer  dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
         >
           <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path>
